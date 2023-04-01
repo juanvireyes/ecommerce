5cbf59dc1a1daced272f3aa5a4f992f938ade9f1
juanvireyes <juanvi1988@gmail.com>
2023-04-01 11:07:25 -0500

    feat: :sparkles: Add email verification and superadmin dashboard refactor
    The app added the email verification feature and it was refactorized the superadmin dashboard in order to separate the users and products management

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\SuperadminController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -9,15 +10,19 @@ Route::get('/', function () {
     return view('home');
 })->name('home');
 
-Route::middleware(['auth', 'can:viewAny,App\Models\User'])->group(function () {
-    Route::get('users', [UserController::class, 'index'])->name('users.index');
-    Route::get('users/{user}', [UserController::class, 'edit'])->name('users.edit');
-    Route::put('users/{user}', [UserController::class, 'update'])->name('users.update');
+Route::middleware(['auth', 'verified', 'can:viewAny,App\Models\User'])->group(function () {
+    Route::get('users', [SuperadminController::class, 'index'])->name('superadmin.index');
+    Route::get('users/{user}', [SuperadminController::class, 'edit'])->name('users.edit');
+    Route::put('users/{user}', [SuperadminController::class, 'update'])->name('users.update');
 });
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('user-dashboard', [UserController::class, 'index'])->name('user.dashboard');
+});
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
