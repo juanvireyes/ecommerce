9ad3b7027b25820a2abe49d1aecc2659afe93ed1
juanvireyes <juanvi1988@gmail.com>
2023-05-04 16:28:57 -0500

    fix: Add CustomComponent to develop

---
+++ resources/js/components/CustomComponent.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="container mx-auto my-auto py-4">
+        <nav class="flex justify-center py-4">
+            <a href="#" class="px-4 py-2 text-gray-700 hover:text-gray-900 hover:underline underline-offset-8 font-bold">Categorías</a>
+            <a href="#" class="px-4 py-2 text-gray-700 hover:text-gray-900 hover:underline underline-offset-8 font-bold">Subcategorías</a>
+            <a href="#" class="px-4 py-2 text-gray-700 hover:text-gray-900 hover:underline underline-offset-8 font-bold">Productos</a>
+          </nav>
+    </div>
+</template>
+
+<script>
+</script>
