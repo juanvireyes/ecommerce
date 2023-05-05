e7b4e3f40ca8605bb1025022856f421f269bf36a
juanvireyes <juanvi1988@gmail.com>
2023-05-05 10:55:44 -0500

    refactor: Refactor RegisteredUserController

---
+++ app/Http/Controllers/RegisteredSuperadminController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\View\View;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Auth\Events\Registered;
+use App\Http\Requests\StoreUserRequest;
+use App\Http\Controllers\Auth\RegisteredUserController;
+
+class RegisteredSuperadminController extends RegisteredUserController
+{
+    public function create(): View
+    {
+        return view('userRegisterLayout');
+    }
+
+    public function store(StoreUserRequest $request): RedirectResponse
+    {
+        $validatedData = $request->validated();
+        $validatedData['name'] = $validatedData['first_name'] . ' ' . $validatedData['last_name'];
+        $validatedData['password'] = Hash::make($validatedData['password']);
+     
+        $user = User::create($validatedData);
+
+        event(new Registered($user));
+
+        $this->assignRolesAndPermissions($user, 'superadmin', ['see users', 'edit users', 'see products', 'edit products', 'delete products', 'see payments', 'approve payments']);
+
+        Auth::login($user);
+
+        return redirect()->route('user.dashboard');
+    }
+
+}
