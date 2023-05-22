7b48d04b59b185017cb0988169c5bbc40a730353
juanvireyes <juanvi1988@gmail.com>
2023-05-21 23:14:02 -0500

    feat: :sparkles: Add shopping cart feature
    Just created the migration of a basic cart. Hasn't been applied yet.

---
+++ database/migrations/2023_05_22_035748_create_carts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations. This migration will create the carts table. The migration hasn't been applied.
+     */
+    public function up(): void
+    {
+        Schema::create('carts', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->boolean('completed')->default(false);
+            $table->dateTime('completed_at')->nullable();
+            $table->decimal('total_amount', 10, 2)->default(0);
+            $table->decimal('discount_amount', 10, 2)->default(0);
+            $table->string('shipping_method')->nullable();
+            $table->string('shipping_address')->nullable();
+            $table->string('billing_address')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('carts');
+    }
+};
